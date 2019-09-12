fdf9b0e43cc5366453289cf4fd8c7ef2dd14b00d
Richard Lee <richlee81192@gmail.com>
2019-09-12 18:09:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,14 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+def bob
+  name = bob
+  age = 11 
+  height = 55
 end 
+
+bob
+
+puts "#{age} this is how old bob is"
