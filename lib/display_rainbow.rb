a91dfe42f1829e8e095546ba6088f530a579eb8d
Richard Lee <richlee81192@gmail.com>
2019-09-12 18:14:57 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,13 +3,13 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
-def bob
+def bob_name
   name = bob
   age = 11 
   height = 55
   return age
 end 
 
-bob
+bob_name
 
 puts "#{age} this is how old bob is"
